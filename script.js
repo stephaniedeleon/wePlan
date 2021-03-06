cd858bab95a050ea16be6c756894356e10530eda
KarinaxRivera <71049969+KarinaxRivera@users.noreply.github.com>
2021-03-06 14:55:49 -0800

    initial commit

--- script.js
+++ script.js
@@ -23,3 +23,73 @@ function signOut() {
         console.log('User signed out.');
     });
 }
+
+import LocalStorage from './LocalStorage.js';
+
+const storage = new LocalStorage();
+const tasks = storage.tasks;
+
+const container = document.querySelector('.tasks');
+const template = document.querySelector('#task');
+
+const createTaskForm = document.querySelector('.create-task');
+const creatTaskField = document.querySelector('.create-task_textarea');
+const createTaskButton = document.querySelector('.create-task_submit');
+
+function onCreateTask({data}) {
+  const clone = template.contect.cloneNode(true);
+
+  const task = clone.querySelector('.task');
+  const checkbox = clone.querySelector('.task_checkbox');
+  const title = clone.querySelector('.task_text');
+  const del = clone.querySelector('.task delete');
+
+  title.innerHTML = data.value;
+  checkbox.checked = data.checked;
+
+  toggleTaskStatusClass({checked:data.checked, task});
+
+  checkbox.addEventListener('input', () => {
+    data.checked = checkbox.checked;
+
+    toggleTasksStatusClass({checked: data.checked, task};
+
+      storage.update(data);
+  });
+   title.addEventListener('input', () => {
+     data.value = title.innerHTML;
+
+     storage.update(data);
+   });
+
+   del.addEventListener('click', (e) => {
+     storage.delete(data);
+     task.remove();
+   });
+
+   container.appendChild(clone);
+}
+
+function toggleTaskStatusClass({checked, task}){
+  task.classList[checked ? 'add' : 'remove']('task--done');
+}
+
+tasks.forEach((data) => {
+  onCreateTask({data});
+});
+
+createTaskForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const value = createTaskField.value;
+
+  if(value){
+    const data = {
+      value;
+      checked: false
+    };
+    storage.create(data);
+    onCreateTask({data});
+    createTaskForm.reset();
+  }
+});
