b180262794c0b1ba50201f6a299483794ddd1569
kimberly-kao <38386920+kimberly-kao@users.noreply.github.com>
2021-03-06 17:53:40 -0800

    changed nav bar to tabs

--- script.js
+++ script.js
@@ -36,15 +36,3 @@ function signOut() {
     welcomeEl.innerText = "";
 }
 
-function w3_open() {
-    document.getElementById("menu").style.marginLeft = "25%";
-    document.getElementById("mySidebar").style.width = "25%";
-    document.getElementById("mySidebar").style.display = "block";
-    document.getElementById("openNav").style.display = 'none';
-}
-function w3_close(){
-    document.getElementById("menu").style.marginLeft = "0%";
-    document.getElementById("mySidebar").style.display = "none";
-    document.getElementById("openNav").style.display = "inline-block";
-}
-
