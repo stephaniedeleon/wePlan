43ca10d0ba2598061813b4531bd57ba0be86e8e3
stephaniedeleon <54347952+stefrdeleon3@users.noreply.github.com>
2021-03-06 21:59:24 -0800

    Updated UI

--- script.js
+++ script.js
@@ -42,6 +42,7 @@ function signOut() {
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
+
 for (i = 0; i < myNodelist.length; i++) {
     var span = document.createElement("SPAN");
     var txt = document.createTextNode("\u00D7");
@@ -60,13 +61,6 @@ for (i = 0; i < close.length; i++) {
     }
 }
 
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
 
 // Create a new list item when clicking on the "Add" button
 function newElement() {
